87bf105169490343c3ba0454345eecf8a79f0b09
Holovachko <holovachko.vasyl@student.uzhnu.edu.ua>
2021-11-11 12:28:10 +0200

    1

--- schoolsite/src/pages/JournalPage.vue
+++ schoolsite/src/pages/JournalPage.vue
@@ -10,27 +10,6 @@ import Journal from '@/components/Journal'
 
     export default {
         name: 'JournalPage',
-        data() {
-            return {
-                marksJournal : [ // просто масив для перевірки чи всьо працює коректно (патом ця інфа буде братися з бази данних) 
-                    {
-                        id : 1,
-                        SubjectName : 'Mathematic',
-                        TeacherName: 'Pogorilack Oleksandr',
-                        StudentName: 'Vasya Holovachko',
-                        StudentMark: '100'
-                    },
-                    {
-                        id : 2,
-                        SubjectName : 'English',
-                        TeacherName: 'Otika Minda',
-                        StudentName: 'Vasya Holovachko',
-                        StudentMark: '95'
-                    }
-                ]
-                
-            }
-        },
         components:{
             MasterPage,
             Journal
