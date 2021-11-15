598d79b03df889cea1ef42a48198232829da5dee
Holovachko <holovachko.vasyl@student.uzhnu.edu.ua>
2021-11-15 13:34:36 +0200

    JrnIt

--- server/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const PORT = 5000;
-const app = express()
-
-app.get('/',(res, req) => {
-    res.status(200).json('сервер працює')
-})
-app.listen(PORT, ()=> console.log('SERVER WORKING'))
